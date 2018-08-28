70aa0c4f4e4bb1ee4348c61ddc3147de69ce13b2
Clinton Jones <1557964@NOAM.TCS.com>
2018-08-28 14:38:57 -0400

    Changes to background colors in scss, animation.ts file, animation components added to app.modules, & bootstrap class changes

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -4,7 +4,8 @@ import {
   state,
   transition,
   style,
-  query
+  query,
+  group
 } from '@angular/animations';
 
 export const fadeAnimation = trigger('fadeAnimation', [
@@ -33,18 +34,28 @@ export const fadeAnimation = trigger('fadeAnimation', [
     )
   ])
 ]);
-export const slideAnimation = trigger('slideAnimation', [
-    transition('* => *', [
-  // The '* => *' will trigger the animation to change between any two states
-    query(
-      ':enter',
-      [style({ transform: 'translateX(100%)'}),animate(50, style({transform: 'translateX(100%)'})) ],
-      { optional: true }
-    ),
-     query(
-      ':leave',
-      [style({ transform: 'translateX(-100%)'}),animate(50, style({transform: 'translateX(-100%)'})) ],
-      { optional: true }
-    ),
-])
-]);
+export const slideAnimation =  trigger('slideAnimation', [
+      transition('* <=> *', [
+        group([
+          query(
+            ':enter',
+            [
+              style({
+                opacity: 0,
+                transform: 'translateY(1rem) rotate(-1deg)'
+              }),
+              animate(
+                '0.35s cubic-bezier(0, 1.8, 1, 1.8)',
+                style({ opacity: 1, transform: 'translateY(0) rotate(0)' })
+              ),
+            ],
+            { optional: true }
+          ),
+          query(
+            ':leave',
+            [animate('0.35s', style({ opacity: 0 }))],
+            { optional: true }
+          )
+        ])
+      ])
+    ]); 
