53401b0d34c962d12530237344ee143e37954bd0
Yijie Tong <1557961@NOAM.TCS.com>
2018-09-04 10:20:41 -0400

    Redirect user to login page after refreshing the page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { CommonService } from '../service/common.service';
 import { Store, State } from '@ngrx/store';
 import { AppState } from '../store/state';
-import { AppActions, AppActionLogout } from '../store/action';
+import { AppActions, AppActionLogout, AppActionAssignOwner } from '../store/action';
 import { Router } from '@angular/router';
 import { fadeAnimation } from '../animations';
 import { slideAnimation } from '../animations';
@@ -39,6 +39,26 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     //show the add component by default when user first logged in
     this.onAdd();
+
+    /** 
+    everytime the user refreshes the page, we want the owner of this session to be automatically assigned
+    with the username stored in the session storage
+
+      if(sessionStorage.getItem("user")){
+        console.log("it is stored");
+        //assign the owner in the state to the username stored in the session storage
+        this.store.dispatch(new AppActionAssignOwner(sessionStorage.getItem("user")));
+      } 
+
+      but this is not safe! User can edit the username information in the session storage
+    */
+
+    //If the owner information of the current state is empty, redirect user to the login page
+    this.store.select("AppReducer").subscribe(state => {
+      if (state.owner === ""){
+        this.router.navigateByUrl("/login");
+      }
+    })
   }
 
   //invoked when the user clicks view tab
@@ -59,6 +79,8 @@ export class DashboardComponent implements OnInit {
 
   //invoked when the user clicks logout button
   onLogout() {
+    //clear the username stored in the session storage
+    sessionStorage.clear();
     //dispatch a logout action to rest the state
     this.store.dispatch(new AppActionLogout());
     this.router.navigateByUrl('/login');
