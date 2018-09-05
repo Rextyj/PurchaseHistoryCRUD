843580b8d6daf5dd821186b65a5dcc327cb353fc
Clinton Jones <1557964@NOAM.TCS.com>
2018-09-05 16:48:00 -0400

    various quality of life linting across files, implementing mixins file, more bootstrap classes, moved router to template in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,16 @@ import { fadeAnimation } from './animations';
 import { slideAnimation } from './animations';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:
+  `<main [@fadeAnimation]="o.isActivated ? o.activatedRoute : ''">
+    <router-outlet #o="outlet">
+    </router-outlet>`,
+  // templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ],
   providers: [],
   animations: [fadeAnimation, slideAnimation]
-  
 })
 export class AppComponent {
   name = 'Angular';
-
 }
+
--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -9,7 +9,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
  * Also provides the ability to search for records between dates defined by user 
  */
 @Component({
-    templateUrl: './summary.component.html'
+    templateUrl: './summary.component.html',
 })
 
 export class SummaryComponent implements OnInit {
@@ -28,34 +28,34 @@ export class SummaryComponent implements OnInit {
                 beginningDate: [],
                 endDate: []
             }
-        )
+        );
     }
 
     ngOnInit() {
-        //subscribe to the store to get the latest state
+        // subscribe to the store to get the latest state
         this.store.select('AppReducer').subscribe(state => {
             console.log('get owner', state.owner);
-            //get the owner information
+            // get the owner information
             this.owner = state.owner;
             console.log('state changed detected');
             console.log('state is', state);
-            //get the summary data
+            // get the summary data
             this.dataToDisplay = state.summary;
         });
-        //dispatch the action to update the summary table
+        // dispatch the action to update the summary table
         this.store.dispatch(new AppActionUpdateSummary({ owner: this.owner }));
     }
 
-    //invoked when show button is clicked
+    // invoked when show button is clicked
     dateBetween(dateForm) {
-        //get the records between the dates
+        // get the records between the dates
         this.service.getBetweenDate(this.owner,dateForm).subscribe(data => {
             this.betweenDate = data;
         });
 
     }
 
-    //invoked when download button is clicked
+    // invoked when download button is clicked
     onDownload() {
         this.res = this.service.convertToCSV(this.dataToDisplay);
         var blob = new Blob([this.res], { type: 'application/summary' });
