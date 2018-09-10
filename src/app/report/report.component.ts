2eb1b5c6874c0a443cec38f1d069c6b923a2e535
Yijie Tong <1557961@NOAM.TCS.com>
2018-09-10 09:42:25 -0400

    Fixed the issue that the bar graph is not correctly displaying data and labels

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -10,31 +10,34 @@ import { AppState } from '../store/state';
 })
 export class ReportComponent implements OnInit {
 
-  monthlyData:number[] = [];
+  monthlyData: number[] = [];
   companyData = [];
   owner;
-  // lineChart
-  public lineChartData:Array<any> = [{data: []}];
-  public lineChartLabels:Array<any> = [];
-  public barChartData:Array<any> = [{data: []}];
-  public barChartLabels:Array<any> = [];
+
+  // lineChart variables
+  public lineChartData: Array<any> = [{ data: [] }];
+  public lineChartLabels: Array<any> = [];
+  //barchart variables
+  public barChartData: Array<any> = [{ data: [] }];
+  public barChartLabels: Array<any> = [];
+
   constructor(private service: ProductInterfaceImpl, private store: Store<AppState>) { }
 
   ngOnInit() {
+    //grab the owner info on init and show the company grah automatically
     this.store.select('AppReducer').subscribe(state => {
       this.owner = state.owner;
       this.onShowByCompany();
     });
   }
 
-  
-  
-  public lineChartOptions:any = {
+  //---------------------------------Configuring linechart-------------------------------------
+  public lineChartOptions: any = {
     responsive: true,
     scales: {
       xAxes: [{
         scaleLabel: {
-          display:true,
+          display: true,
           labelString: 'Month'
         }
       }],
@@ -46,7 +49,8 @@ export class ReportComponent implements OnInit {
       }]
     }
   };
-  public lineChartColors:Array<any> = [
+
+  public lineChartColors: Array<any> = [
     { // grey
       backgroundColor: 'rgba(148,159,177,0.2)',
       borderColor: 'rgba(148,159,177,1)',
@@ -55,72 +59,41 @@ export class ReportComponent implements OnInit {
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
-    //,
-    // { // dark grey
-    //   backgroundColor: 'rgba(77,83,96,0.2)',
-    //   borderColor: 'rgba(77,83,96,1)',
-    //   pointBackgroundColor: 'rgba(77,83,96,1)',
-    //   pointBorderColor: '#fff',
-    //   pointHoverBackgroundColor: '#fff',
-    //   pointHoverBorderColor: 'rgba(77,83,96,1)'
-    // },
-    // { // grey
-    //   backgroundColor: 'rgba(148,159,177,0.2)',
-    //   borderColor: 'rgba(148,159,177,1)',
-    //   pointBackgroundColor: 'rgba(148,159,177,1)',
-    //   pointBorderColor: '#fff',
-    //   pointHoverBackgroundColor: '#fff',
-    //   pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    // }
   ];
-  public lineChartLegend:boolean = false;
-  public lineChartType:string = 'line';
- 
-  // public randomize():void {
-  //   let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-  //   for (let i = 0; i < this.lineChartData.length; i++) {
-  //     _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-  //     for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-  //       _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-  //     }
-  //   }
-  //   this.lineChartData = _lineChartData;
-  // }
- 
-  // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
- 
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
+  public lineChartLegend: boolean = false;
+  public lineChartType: string = 'line';
 
-  onShowMonthly(year){
+  //push data returned from server to the arrays to display
+  onShowMonthly(year) {
     this.service.getMonthlyData(this.owner, year).subscribe(_data => {
       console.log("monthly data is ", _data);
       this.monthlyData = [];
       this.lineChartLabels.length = 0;
-      for(let row of _data){
+      for (let row of _data) {
         this.monthlyData.push(row[1]);
         this.lineChartLabels.push(row[0]);
       }
       console.log("data is ", this.monthlyData);
       console.log('label is ', this.lineChartLabels);
       this.lineChartData = [
-        {data: this.monthlyData}
+        { data: this.monthlyData }
       ];
     });
   }
 
-  public barChartOptions:any = {
+  //-------------------------------------configuring barchart---------------------------------------
+  public barChartOptions: any = {
     scaleShowVerticalLines: false,
     responsive: true,
     scales: {
       xAxes: [{
         scaleLabel: {
-          display:true,
+          display: true,
           labelString: 'Company Name'
+        },
+        //show every xAxes labels
+        ticks: {
+          autoSkip: false
         }
       }],
       yAxes: [{
@@ -131,26 +104,32 @@ export class ReportComponent implements OnInit {
       }]
     }
   };
-  // public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = false;
- 
-  // public barChartData:any[] = [
-  //   {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-  //   {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  // ];
 
-  onShowByCompany(){
+  public barChartType: string = 'bar';
+  public barChartLegend: boolean = false;
+
+  //push data returned from server to the arrays to display
+  onShowByCompany() {
     this.service.getCompanyData(this.owner).subscribe(_data => {
       console.log("monthly data is ", _data);
-      for(let row of _data){
+      for (let row of _data) {
         this.companyData.push(row[1]);
         this.barChartLabels.push(row[0]);
       }
       console.log("data is ", this.monthlyData);
       this.barChartData = [
-        {data: this.barChartLabels}
+        { data: this.companyData }
       ];
     });
+  }
+
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
   }
 }
